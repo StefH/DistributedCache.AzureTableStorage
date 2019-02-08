68509f70946f9c5c74b52e0db4a1c5a27fee7436
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2019-02-08 14:13:21 +0100

    nuget

---
+++ src/DistributedCache.AzureTableStorage/Interfaces/IDistributedCache`1.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DistributedCache.AzureTableStorage.Interfaces
+{
+    public interface IDistributedCache<T> : IDistributedCache where T : class
+    {
+        Task<T> GetItemAsync(string key, CancellationToken token = default(CancellationToken));
+
+        Task SetItemAsync(string key, T value, DistributedCacheEntryOptions options, CancellationToken token = default(CancellationToken));
+
+        Task RefreshItemAsync(string key, CancellationToken token = default(CancellationToken));
+
+        Task RemoveItemAsync(string key, CancellationToken token = default(CancellationToken));
+    }
+}
