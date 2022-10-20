e330f96052e8a123dae26ed602f01a4d7aa07913
Stef Heyenrath <stef.heyenrath@gmail.com>
2022-10-20 18:02:34 +0200

    update icon

--- Generate-ReleaseNotes.cmd
+++ Generate-ReleaseNotes.cmd
@@ -1,5 +1,5 @@
 rem https://github.com/StefH/GitHubReleaseNotes
 
-SET version=3.0.1
+SET version=3.1.0
 
 GitHubReleaseNotes --output CHANGELOG.md --skip-empty-releases --exclude-labels question invalid doc duplicate --version %version% --token %GH_TOKEN%
