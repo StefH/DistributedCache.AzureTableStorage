e35bb6b6f09fb4497b2d331935279f5249c601c9
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2022-10-21 14:25:33 +0200

    Add support for ManagedIdentity (#16)
    * Add support for ManagedIdentity
    
    * .
    
    * abstract
    
    * .
    
    * .
    
    * Fix
    
    * 2
    
    * csproj
    
    * update icon

--- src/DistributedCache.AzureTableStorage.V3/Models/CachedItem.cs
+++ src/DistributedCache.AzureTableStorage.V3/Models/CachedItem.cs
@@ -2,72 +2,67 @@
 using Azure;
 using Azure.Data.Tables;
 
-namespace DistributedCache.AzureTableStorage.Models
+namespace DistributedCache.AzureTableStorage.Models;
+
+/// <summary>
+/// Represents an item to be stored in the table.
+/// </summary>
+public class CachedItem : ITableEntity
 {
+    public string PartitionKey { get; set; } = null!;
+
+    public string RowKey { get; set; } = null!;
+
+    public DateTimeOffset? Timestamp { get; set; }
+
+    public ETag ETag { get; set; }
+
     /// <summary>
-    /// Represents an item to be stored in the table.
+    /// Gets or sets the data.
     /// </summary>
-    public class CachedItem : ITableEntity
-    {
-#pragma warning disable CS8618
-        public string PartitionKey { get; set; }
+    public byte[]? Data { get; set; }
 
-        public string RowKey { get; set; }
-#pragma warning restore CS8618
+    ///// <summary>
+    ///// Gets or sets the sliding expiration in ticks.
+    ///// </summary>
+    //public long? SlidingExpirationTicks { get; set; }
 
-        public DateTimeOffset? Timestamp { get; set; }
+    ///// <summary>
+    ///// Gets or sets the sliding expiration as a <see cref="TimeSpan"/>.
+    ///// </summary>
+    ///// <remarks>
+    ///// Ignored as <see cref="TimeSpan"/> is not supported in Azure Table Storage.
+    ///// </remarks>
+    //[Ignore]
+    //public TimeSpan? SlidingExpiration
+    //{
+    //    get
+    //    {
+    //        if (SlidingExpirationTicks.HasValue)
+    //        {
+    //            return TimeSpan.FromTicks(SlidingExpirationTicks.Value);
+    //        }
 
-        public ETag ETag { get; set; }
+    //        return null;
+    //    }
 
-        /// <summary>
-        /// Gets or sets the data.
-        /// </summary>
-        public byte[]? Data { get; set; }
+    //    set
+    //    {
+    //        if (value.HasValue)
+    //        {
+    //            SlidingExpirationTicks = value.Value.Ticks;
+    //        }
+    //    }
+    //}
 
-        ///// <summary>
-        ///// Gets or sets the sliding expiration in ticks.
-        ///// </summary>
-        //public long? SlidingExpirationTicks { get; set; }
+    /// <summary>
+    /// The absolute expiration date for the cache entry.
+    /// This property is named 'AbsoluteExpiration' to be backwards compatible, a better name would be 'ExpiresAtTime'.
+    /// </summary>
+    public DateTimeOffset AbsoluteExpiration { get; set; }
 
-        ///// <summary>
-        ///// Gets or sets the sliding expiration as a <see cref="TimeSpan"/>.
-        ///// </summary>
-        ///// <remarks>
-        ///// Ignored as <see cref="TimeSpan"/> is not supported in Azure Table Storage.
-        ///// </remarks>
-        //[Ignore]
-        //public TimeSpan? SlidingExpiration
-        //{
-        //    get
-        //    {
-        //        if (SlidingExpirationTicks.HasValue)
-        //        {
-        //            return TimeSpan.FromTicks(SlidingExpirationTicks.Value);
-        //        }
-
-        //        return null;
-        //    }
-
-        //    set
-        //    {
-        //        if (value.HasValue)
-        //        {
-        //            SlidingExpirationTicks = value.Value.Ticks;
-        //        }
-        //    }
-        //}
-
-        /// <summary>
-        /// The absolute expiration date for the cache entry.
-        /// This property is named 'AbsoluteExpiration' to be backwards compatible, a better name would be 'ExpiresAtTime'.
-        /// </summary>
-        public DateTimeOffset AbsoluteExpiration { get; set; }
-
-        /// <summary>
-        /// Gets or sets the date and time the item was last accessed.
-        /// </summary>
-        public DateTimeOffset LastAccessTime { get; set; }
-
-
-    }
+    /// <summary>
+    /// Gets or sets the date and time the item was last accessed.
+    /// </summary>
+    public DateTimeOffset LastAccessTime { get; set; }
 }
--- src/DistributedCache.AzureTableStorage/Utils/BinarySerializer.cs
+++ src/DistributedCache.AzureTableStorage/Utils/BinarySerializer.cs
@@ -4,41 +4,40 @@ using System.Linq;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Bson;
 
-namespace DistributedCache.AzureTableStorage.Utils
+namespace DistributedCache.AzureTableStorage.Utils;
+
+internal static class BinarySerializer
 {
-    internal static class BinarySerializer
+    public static byte[]? Serialize(object? obj)
     {
-        public static byte[]? Serialize(object? obj)
+        if (obj == null)
         {
-            if (obj == null)
-            {
-                return null;
-            }
-
-            var memoryStream = new MemoryStream();
-            using var writer = new BsonDataWriter(memoryStream);
-            var serializer = new JsonSerializer();
-            serializer.Serialize(writer, obj);
-
-            return memoryStream.ToArray();
+            return null;
         }
 
-        public static T? Deserialize<T>(byte[]? data) where T : class
+        var memoryStream = new MemoryStream();
+        using var writer = new BsonDataWriter(memoryStream);
+        var serializer = new JsonSerializer();
+        serializer.Serialize(writer, obj);
+
+        return memoryStream.ToArray();
+    }
+
+    public static T? Deserialize<T>(byte[]? data) where T : class
+    {
+        if (data == null)
         {
-            if (data == null)
-            {
-                return default;
-            }
-
-            var memoryStream = new MemoryStream(data);
-            using var reader = new BsonDataReader(memoryStream);
-            if (typeof(T).GetInterfaces().Any(t => t.IsGenericType && t.GetGenericTypeDefinition() == typeof(IEnumerable<>)))
-            {
-                reader.ReadRootValueAsArray = true;
-            }
-
-            var serializer = new JsonSerializer();
-            return serializer.Deserialize<T>(reader);
+            return default;
         }
+
+        var memoryStream = new MemoryStream(data);
+        using var reader = new BsonDataReader(memoryStream);
+        if (typeof(T).GetInterfaces().Any(t => t.IsGenericType && t.GetGenericTypeDefinition() == typeof(IEnumerable<>)))
+        {
+            reader.ReadRootValueAsArray = true;
+        }
+
+        var serializer = new JsonSerializer();
+        return serializer.Deserialize<T>(reader);
     }
 }
